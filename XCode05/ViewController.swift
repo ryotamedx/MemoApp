e0e9d71fb64487e05d97dfc3dcdaef5858d8f21b
ryotamedy <ryotamex2@gmail.com>
2021-11-02 13:16:21 +0900

    Initial Commit

---
+++ XCode05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCode05
+//
+//  Created by 溜田遼 on 2021/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
